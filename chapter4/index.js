686ca39f6db59f786dcde76718ac3f0fcd71300c
Alejandro Butron <alesvoy@gmail.com>
2021-06-29 22:50:17 -0500

    Solved first problem from chapter 4

---
+++ chapter4/index.js
@@ -0,0 +1,49 @@
+// Sum of a range
+
+// function range (start, end, step = 1){
+//     let result = [start];
+
+//     if(step < 0){
+//         while(result[result.length - 1] > end){
+//             start += step;
+//             result.push(start);
+//             if(result[result.length - 1] < end){
+//                 result.pop();
+//                 return result;
+//             }
+//         }
+        
+//         return result;
+//     }
+    
+//     while(result[result.length - 1] < end){
+//         start += step;
+//         result.push(start);
+//         if(result[result.length - 1] > end){
+//             result.pop()
+//             return result;
+//         }
+//     }
+    
+//     return result;
+
+// }
+
+// function sumArrays(arr){
+//     let sum = 0;
+
+//     for(let i = 0; i < arr.length; i++){
+//         sum += arr[i]
+//     }
+
+//     return sum;
+// }
+
+// let test = range(5,2, -1);
+
+// for(let i = 0; i < test.length; i++){
+//     console.log(test[i]);
+// }
+
+// Reversing an Array
+
